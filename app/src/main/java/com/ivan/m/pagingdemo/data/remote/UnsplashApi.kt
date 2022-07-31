4098659d3afefd3cd8948cf3fb52baecdefb4552
ivancity <shieldm@hotmail.com>
2022-07-31 17:53:47 -0500

    making API interface for the network requests

---
+++ app/src/main/java/com/ivan/m/pagingdemo/data/remote/UnsplashApi.kt
@@ -0,0 +1,26 @@
+package com.ivan.m.pagingdemo.data.remote
+
+import com.ivan.m.pagingdemo.BuildConfig
+import com.ivan.m.pagingdemo.model.SearchResult
+import com.ivan.m.pagingdemo.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("query") query: String,
+        @Query("per_page") perPage: Int
+    ): SearchResult
+
+}
