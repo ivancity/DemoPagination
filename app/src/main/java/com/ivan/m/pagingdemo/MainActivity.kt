69305311a9e4ae68b848ed5da72a327cb5f549c0
ivancity <shieldm@hotmail.com>
2022-08-06 22:16:51 -0500

    Modifying the MainActivity in order to SetupNavGraph, this will create a NavHost that will call the HomeScreen composable. Also adding a HomeViewModel in order to call the repo.getAllImages and to subscribe to its flow result.

--- app/src/main/java/com/ivan/m/pagingdemo/MainActivity.kt
+++ app/src/main/java/com/ivan/m/pagingdemo/MainActivity.kt
@@ -10,24 +10,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.ExperimentalPagingApi
+import coil.annotation.ExperimentalCoilApi
+import com.ivan.m.pagingdemo.navigation.SetupNavGraph
 import com.ivan.m.pagingdemo.ui.theme.PagingDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalCoilApi
+@ExperimentalPagingApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PagingDemoTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    PagingDemoTheme {
-//    }
-//}
---
+++ app/src/main/java/com/ivan/m/pagingdemo/screens/home/HomeScreen.kt
@@ -0,0 +1,20 @@
+package com.ivan.m.pagingdemo.screens.home
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.compose.collectAsLazyPagingItems
+import coil.annotation.ExperimentalCoilApi
+
+@ExperimentalCoilApi
+@ExperimentalPagingApi
+@Composable
+fun HomeScreen(
+    navController: NavHostController,
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val getAllImages = homeViewModel.getAllImages.collectAsLazyPagingItems()
+
+}
